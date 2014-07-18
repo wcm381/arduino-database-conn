2cbe2d6c73c202e81ebafcbc4e8a4bb3a3066377
wcm381 <wcm381@gmail.com>
2014-07-18 11:49:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Just testing creating a table, inserting to it and getting values from it
+
+
 // Load Modules
 var http = require('http'),
     net = require('net'),
@@ -13,17 +16,56 @@ var HTTP_PORT = 8080;
 
 
 
-// Configure Connection and Connect to SQL Database
+// Configure Connection to SQL Database
 var connection = mysql.createConnection({
-	host     : 'dbinstancesql.c7tazdmnvenv.us-east-1.rds.amazonaws.com;dbname=mydbSQL1',
-	user     : 'userWill',
+	host     : 'xxxxxxx',
+	user     : 'xxxxxxx',
 	password : 'xxxxxxxx',
 	port 	   : '3306'
 });
 
 
-// 
+// Connect to SQL Database
+connection.connect(function(err) {
+  // connected unless `err` is set
+});
 
+// Don't need connection.query('USE' ...  ?
 
+// Create Table
+connection.query( 'CREATE TABLE IF NOT EXISTS sampleTbl (' +
+		'P_Id  INT	 NOT NULL AUTO_INCREMENT,'  +
+		'Time  DATETIME  NOT NULL,'  +
+		'Temp  FLOAT	 NOT NULL,'  +
+		'PRIMARY KEY(P_Id)'	     +
+		')', function( err ){
+    			if ( err )  throw err;
+    			console.log( 'Table created.' );
+		});
+		
+					
+// Insert to Table
 
+var sampleSet = { Time: '2014-07-17 23:16:00', Temp: 23.1 };
 
+connection.query( 'INSERT INTO sampleTbl SET ?', sampleSet, function(err, result) {
+	if ( err )  throw err;
+	console.log(results);
+});
+
+		
+
+					
+// Check Table for Row I just put In					
+client.query('SELECT * FROM sampleTbl', function (err, rows, result) {
+	if ( err )  throw err;
+	for (var i in rows) {
+		console.log('Temp Values: ', rows[i].Temp);	
+	}
+
+// End Connection to Database
+connection.end();			
+					
+					
+					
+					
